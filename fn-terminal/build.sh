c88aa64245415ace01aa109d9e61c52f39f7e22d
Ing <wjz304@qq.com>
2026-01-29 19:37:33 +0800

    fix: 更新fn-terminal，使用 env bash 以提高兼容性

--- fn-terminal/build.sh
+++ fn-terminal/build.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Copyright (C) 2022 Ing <https://github.com/wjz304>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
 WORKDIR="$(
   cd "$(dirname "$0")"
